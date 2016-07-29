8814b6cdabed6b9b7138fd952eee1d1d21695b01
vcolavin <vcolavin@gmail.com>
2016-07-29 15:34:00 -0700

    progress bar, and submitting onclick work

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,19 @@
 $(function () {
   $('#new_video').fileupload({
-    done: function(e, data){},
+    // done: function(e, data){},
+    dataType: "json",
+    add: function (e, data) {
+      $("#submit-video").on('click', function () {
+          data.submit();
+        })
+      },
     progressall: function(e, data) {
       var progress = parseInt(data.loaded / data.total * 100, 10);
-      $('#progress .bar').css('width', progress + '%')
+      $('.progress .bar').css('width', progress + '%')
+    }
+    // done: function(e, data) {
+      // follow redirect
+    // }
     }
   })
 })
